47c3118e9d8dc93e196566b363df47135fccfd42
cozyCr4te <devsepnine@hotmail.com>
2025-02-17 17:25:39 +0900

    feat: init

---
+++ package/prettier-config/index.mjs
@@ -0,0 +1,15 @@
+
+const config = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  arrowParens: "avoid",
+  endOfLine: "lf",
+  jsxSingleQuote: true,
+};
+
+export default config;
